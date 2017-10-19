9c3b1af3e5110a41cb1ff6a2464bdb377ad9a5d0
Max Bureck <max.bureck@fokus.fraunhofer.de>
2017-10-19 11:55:31 +0200

    Adjusted gitignore

--- tests/de.fhg.fokus.xtensions.bench/src/main/java/de/fhg/fokus/xtensions/bench/RangeBenchmark.java
+++ tests/de.fhg.fokus.xtensions.bench/src/main/java/de/fhg/fokus/xtensions/bench/RangeBenchmark.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2017 Max Bureck (Fraunhofer FOKUS) and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- *
- * Contributors:
- *     Max Bureck (Fraunhofer FOKUS) - initial API and implementation
- *******************************************************************************/
 package de.fhg.fokus.xtensions.bench;
 
 import java.util.function.IntPredicate;
