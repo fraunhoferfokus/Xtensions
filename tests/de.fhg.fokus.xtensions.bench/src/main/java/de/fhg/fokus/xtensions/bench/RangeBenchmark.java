00152da03819daebb399dd74eea2082f441de4ec
Max Bureck <max.bureck@fokus.fraunhofer.de>
2018-07-24 11:52:56 +0200

    Updated version ids to upcoming version 1.2.0

--- tests/de.fhg.fokus.xtensions.bench/src/main/java/de/fhg/fokus/xtensions/bench/RangeBenchmark.java
+++ tests/de.fhg.fokus.xtensions.bench/src/main/java/de/fhg/fokus/xtensions/bench/RangeBenchmark.java
@@ -6,12 +6,11 @@ import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.eclipse.xtext.xbase.lib.IntegerRange;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.openjdk.jmh.annotations.Benchmark;
 
 @SuppressWarnings("all")
 public class RangeBenchmark {
-  @Benchmark
-  public void testRange() {
+  /* @Benchmark
+   */public void testRange() {
     final Function1<Integer, Boolean> _function = (Integer it) -> {
       return Boolean.valueOf((((it).intValue() % 2) == 0));
     };
@@ -21,8 +20,8 @@ public class RangeBenchmark {
     IterableExtensions.<Integer, Integer>fold(IterableExtensions.<Integer>filter(new IntegerRange(0, 1_000_000), _function), Integer.valueOf(0), _function_1);
   }
   
-  @Benchmark
-  public void testStream() {
+  /* @Benchmark
+   */public void testStream() {
     final IntPredicate _function = (int it) -> {
       return ((it % 2) == 0);
     };
